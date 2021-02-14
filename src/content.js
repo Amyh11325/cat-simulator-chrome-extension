0933d4bd101f5ca15ed28d1b4323a1d5186e0e88
nhs-wobuzhidao <anoushka@ucsb.edu>
2021-02-13 20:34:01 -0800

    add options menu

--- src/content.js
+++ src/content.js
@@ -1,15 +1,28 @@
-var overlay = document.createElement("div");
-var cat = document.createElement("div");
-var catWrap = document.createElement("div");
+/* File: content.js
+ * ---------------
+ * Hello! You'll be making most of your changes
+ * in this file. At a high level, this code replaces
+ * the substring "cal" with the string "butt" on web pages.
+ *
+ * This file contains javascript code that is executed
+ * everytime a webpage loads over HTTP or HTTPS.
+ */
 
-var catTiles = chrome.extension.getURL("src/assets/cat_tiles.png");
+var elements = document.getElementsByTagName('*');
 
-overlay.classList.add("overlay");
-catWrap.classList.add("cat-wrapper");
-cat.classList.add("cat");
-cat.classList.add("walking");
-cat.style.backgroundImage = "url(" + catTiles + ")";
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
 
-catWrap.appendChild(cat);
-overlay.appendChild(catWrap);
-document.body.appendChild(overlay);
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/cal/gi, "butt"); // replaces "cal," "Cal", etc. with "butt"
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+              }
+        }
+    }
+}
